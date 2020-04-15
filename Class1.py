d595bd4a35657ba99c3be6ba0f22546b4402095c
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-16 00:43:52 +0300

    Update Class1.py

--- Class1.py
+++ Class1.py
@@ -1,2 +1,10 @@
-class Serebro():
+class City():
+    def __init__(self,name,plaque):
+        self.name = name
+        self.plaque = plaque
+        
+    # string = City("İstanbul" , 34)
+    # string.name
+    # string.plaque
+        
     
