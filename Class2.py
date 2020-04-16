174cafbc21641bae2320cb2dd24fcdb9410b79b8
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-16 15:58:56 +0300

    Update Class2.py

--- Class2.py
+++ Class2.py
@@ -1,1 +1,14 @@
 class Years():
+  t_year = 12
+  def __init__(self,year=12):
+    self.year = year
+    
+  def newyear(self):
+    return self.year * self.t_year
+  
+  
+  
+  
+  
+  # year  = Years()
+  # year.newyear()
