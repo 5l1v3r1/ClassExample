6f2ced8d0deb98845a3b1096e95eeb555ea1478b
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-16 16:04:26 +0300

    Create Class3.py

---
+++ Class3.py
@@ -0,0 +1,22 @@
+class Class1():
+	def __init__(self):
+		print("Class 1 Created")
+	def method_1(self):
+		print("method 1 ")
+	def method_2(self):
+		print("method 2 ")
+
+
+
+
+class Class2(Class1):
+	def __init__(self):
+		Class1.__init(self)
+		print("Class 2 Created")
+	def method_3(self):
+		print("method 3 ")
+
+
+
+# my_clas = Class2()
+# my_clas.method(1,2,3)
