016c880f8940de5612a61707ea93fab697f9cad5
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-16 16:11:18 +0300

    Update Class4.py

--- Class4.py
+++ Class4.py
@@ -1,1 +1,15 @@
-
+class Fruits():
+    def __init__(self,name,calories):
+        self.name = name
+        self.calories = calories
+        
+    def __str__(self):
+        return f"{self.name} and {self.calories}"
+    
+    def __len__(self):
+        return self.calories
+      
+      
+ ## my_list = Fruits("Banana", 200)
+ ## print(my_list)
+ ## len(my_list
